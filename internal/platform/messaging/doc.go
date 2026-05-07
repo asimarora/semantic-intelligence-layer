78ff3d4305cae291e14049430b6082f0cf6f320e
Asim Arora <asimarora@gmail.com>
2026-05-07 15:43:44 +0530

    Create doc.go

---
+++ internal/platform/messaging/doc.go
@@ -0,0 +1,3 @@
+// Package messaging holds the shared transport abstractions and JetStream adapters
+// that connect SIL services without leaking broker details into business logic.
+package messaging
