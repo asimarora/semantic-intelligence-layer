708db2387ffa217cbb7e4992c1693e0ab49f955b
Asim Arora <asimarora@gmail.com>
2026-05-08 13:08:12 +0530

    Create doc.go

---
+++ internal/transport/chat/doc.go
@@ -0,0 +1,4 @@
+// Package chat contains conversational transport adapters for user-facing chat
+// experiences built on top of SIL's shared query, investigation, and memory
+// services.
+package chat
